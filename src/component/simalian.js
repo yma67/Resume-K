86d2a0ab499df5b0ac84b3efc33ec75b7c855106
yma67 <yuxiang.ma@mail.mcgill.ca>
2019-02-07 01:44:17 -0500

    加入了新的复读词汇以及增加了点击换词以及加大了button

--- src/component/simalian.js
+++ src/component/simalian.js
@@ -5,22 +5,29 @@ import {Link} from "react-router-dom";
 import ProjectRouter from "../ProjectRouter";
 
 export default class Simalian extends Component{
-    state = { load: true };
+    constructor(props) {
+        super(props);
+        this.favoratesOfKing = ['村头烧烤', '可不敢', '太毒了', '稳啊', '高科技', '脸神', '闹呢', '跟着学学', '写method', '笛哥', '笛王', '马校长', '新技术', '太强了', 'Gunter是SE最好的教授'];
+        this.state = { load: true,  text: this.favoratesOfKing[this.rand(0, this.favoratesOfKing.length - 1)]};
+    }
     handleImageLoad() {
         this.setState({load: false});
     }
+    handleRandomOnClick() {
+        this.setState({text: this.favoratesOfKing[this.rand(0, this.favoratesOfKing.length - 1)]});
+    }
+    rand = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
     render() {
-        let favoratesOfKing = ['村头烧烤', '可不敢', '太毒了', '稳啊', '高科技', '脸神', '闹呢'];
-        let rand = (min, max) => {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        };
+
         return(
             <div className="App">
                 <header className="App-header">
                     <Dimmer active={this.state.load} page>
-                        <Header as='h2' icon inverted>
+                        <Header as='h1' icon inverted>
                             <Icon name='smile' />
                             川剧变脸
                             <Header.Subheader>组皇职业照</Header.Subheader>
@@ -29,11 +36,11 @@ export default class Simalian extends Component{
                     <img src={sml} className="App-logo" alt="logo" onLoad={this.handleImageLoad.bind(this)} />
                     <br />
                     <br />
-                    <Header as='h1' icon centered inverted>
-                        {favoratesOfKing[rand(0, favoratesOfKing.length - 1)]}
+                    <Header as='h1' icon centered inverted onClick={this.handleRandomOnClick.bind(this)}>
+                        {this.state.text}
                     </Header>
                     <br />
-                    <Button as={Link} centered inverted color={'black'} to={'/resume'}><code>bossli.io</code></Button>
+                    <Button as={Link} size={'huge'} centered inverted color={'black'} to={'/resume'}><code>bossli.io</code></Button>
                     <ProjectRouter/>
                 </header>
             </div>
