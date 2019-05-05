a3c893deb87d6dda51dffa7eeea77a26eb0cd608
yma67 <yuxiang.ma@mail.mcgill.ca>
2019-05-05 16:14:54 -0400

    TUM2

--- src/model/Work.js
+++ src/model/Work.js
@@ -3,7 +3,7 @@ import tum from '../img/tum.svg';
 
 const Work = {
     workPersistence: [
-        {wid: 0, title: "Research Intern", institute: "Technische Universität München (TUM)", timeRange: "May 2019 - Aug 2019", location: "Germany", relSkill:["Analysis of Commercial Traffic Data Using Various Machine Learning Approaches", "Assisted in Business and Financial Analysis"], imgsrc: tum},
+        {wid: 0, title: "Research Intern", institute: "Technische Universität München (TUM)", timeRange: "May 2019 - Aug 2019", location: "Germany", relSkill:["Data analysis using various Machine Learning Approaches. ", "Assisted in Business and Financial Analysis"], imgsrc: tum},
         {wid: 1, title: "Operations Intern, Private Equity Division", institute: "Zhongtai Securities Co., Ltd.", timeRange: "June 2018 - Aug 2018", location: "Jinan, Shandong China", relSkill:["Collected data from financial data platforms, research reports and financial statements to create charts and forms for company valuation", "Used Excel to categorize and index more than 300 documents, making information search efficient ", "Communicated effectively with other departments, organized meetings and followed up project execution "], imgsrc: wk1},
         {wid: 2, title: "Development Intern, R&D Department", institute: "ZhengChen Inc. ", timeRange: "June 2018, Shandong China", location: "Jinan, Shandong China", relSkill:["ZhengChen Inc. is an innovative artificial intelligence and energy-efficient solutions company"], imgsrc: wk1}
     ],
